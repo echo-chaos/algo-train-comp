0ed08970e1f3bae966a8135619effb37e884962a
git-lql <1805686827@qq.com>
2022-01-09 23:15:06 +0800

    week02-work-和为 K 的子数组

---
+++ src/main/java/org/lql/week02/SubarraySum.java
@@ -0,0 +1,64 @@
+package org.lql.week02;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: lql
+ * @date: 2022/1/3 22:43
+ * @description: 560. 和为 K 的子数组 https://leetcode-cn.com/problems/subarray-sum-equals-k/
+ */
+public class SubarraySum {
+
+    /**
+     * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回该数组中和为 k 的连续子数组的个数。
+     *
+     * <P>示例：</P>
+     * <li>输入：nums = [1,1,1], k = 2</li>
+     * <li>输出：2</li>
+     *
+     * <li>输入：nums = [1,2,3], k = 3</li>
+     * <li>输出：2</li>
+     *
+     * <p>提示: </p>
+     * <li>1 <= nums.length <= 2 * 104</li>
+     * <li>-1000 <= nums[i] <= 1000</li>
+     * <li>-107 <= k <= 107</li>
+     * <li></li>
+     */
+    private static final Logger logger = LoggerFactory.getLogger(SubarraySum.class);
+
+    public int subarraySum(int[] nums, int k) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        // 连续次数->数组元素个数、前缀和
+        int count = 0;
+        int pre = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int num : nums) {
+            pre += num;
+            // pre[i] = pre[i-1] + nums[i]
+            // k -> (j~i)
+            // k -> pre[i] - pre[j-1]
+            // pre[j-1] = pre[i] - k
+            if (map.containsKey(pre - k)) {
+                count += map.get(pre - k);
+            }
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SubarraySum sum = new SubarraySum();
+        int[] nums = {1, 1, 1};
+        int k = 2;
+        int i = sum.subarraySum(nums, k);
+        System.out.println(i);
+    }
+}
