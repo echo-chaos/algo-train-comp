b7d832f19d002435ac4baf0440474b85018b6d9d
git-lql <1805686827@qq.com>
2022-01-09 15:32:48 +0800

    day14-11. 盛最多水的容器

---
+++ src/main/java/org/lql/leetcode/MaxArea.java
@@ -0,0 +1,35 @@
+package org.lql.leetcode;
+
+/**
+ * @author: lql
+ * @date: 2022/1/9 15:19
+ * @description: 11. 盛最多水的容器 https://leetcode-cn.com/problems/container-with-most-water/
+ */
+public class MaxArea {
+
+    public int maxArea(int[] height) {
+        if (height.length < 2) {
+            return 0;
+        }
+        int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            ans = Math.max(ans, area);
+            if (height[left] >= height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        MaxArea maxArea = new MaxArea();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int area = maxArea.maxArea(height);
+        System.out.println(area);
+    }
+}
