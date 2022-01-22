66897304e084adf73c657780a436efeb2ef7b295
git-lql <1805686827@qq.com>
2022-01-23 01:25:38 +0800

    week03-work-从中序与后序遍历序列构造二叉树

--- src/main/java/org/lql/week03/BuildTree.java
+++ src/main/java/org/lql/week03/BuildTree.java
@@ -4,6 +4,9 @@ import org.lql.common.TreeNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author: lql
  * @date: 2022/1/10 22:24
@@ -29,7 +32,37 @@ public class BuildTree {
     private static final Logger logger = LoggerFactory.getLogger(BuildTree.class);
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        // 前序遍历 ：根节点 -> 左子树 -> 右子树
+        // 中序遍历 ：左子树 -> 根节点 -> 右子树
+        // 后序遍历 ：左子树 -> 右子树 -> 根节点
+        // 后续遍历的末位置为根节点
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.push(root);
+        int inorderIndex = inorder.length - 1;
+        for (int i = postorder.length - 2; i >= 0; i--) {
+            int postorderVal = postorder[i];
+            TreeNode node = queue.peek();
+            assert node != null;
+            if (node.val != inorder[inorderIndex]) {
+                node.right = new TreeNode(postorderVal);
+                queue.push(node.right);
+            } else {
+                while (!queue.isEmpty() && queue.peek().val == inorder[inorderIndex]) {
+                    node = queue.pop();
+                    inorderIndex--;
+                }
+                node.left = new TreeNode(postorderVal);
+                queue.push(node.left);
+            }
+        }
+        return root;
+    }
 
-        return null;
+    public static void main(String[] args) {
+        BuildTree buildTree = new BuildTree();
+        int[] inorder = {9,3,15,20,7};
+        int[] postorder = {9,15,7,20,3};
+        TreeNode treeNode = buildTree.buildTree(inorder, postorder);
     }
 }
