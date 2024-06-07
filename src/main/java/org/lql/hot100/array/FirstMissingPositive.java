b620ca2efe00b35f371dca9a137fd363a535500a
chaos <1805686827@qq.com>
2024-06-07 15:50:07 +0800

    hot 100 修改

--- src/main/java/org/lql/hot100/array/FirstMissingPositive.java
+++ src/main/java/org/lql/hot100/array/FirstMissingPositive.java
@@ -56,6 +56,33 @@ public class FirstMissingPositive {
         return n + 1;
     }
 
+    public int firstMissingPositive1(int[] nums) {
+        int n = nums.length;
+        // 将数组中所有非正整数替换为一个大于数组长度的正整数，避免与数组长度及以内正整数混淆。
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        // 遍历数组，通过标记对应位置的元素为负数来表示该正整数存在。
+        // 使用绝对值操作确保遍历过程中不会因为负数而出现错误。
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        // 遍历数组找到第一个正数对应的索引，即为缺失的第一个正整数。
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        // 如果数组中所有位置都被标记为负数，则返回数组长度加一。
+        return n + 1;
+    }
+
+
     public static void main(String[] args) {
         FirstMissingPositive solution = new FirstMissingPositive();
         // 2
@@ -64,5 +91,11 @@ public class FirstMissingPositive {
         System.out.println(solution.firstMissingPositive(new int[]{1, 2, 0}));
         // 1
         System.out.println(solution.firstMissingPositive(new int[]{7, 8, 9, 11, 12}));
+        // 2
+        System.out.println(solution.firstMissingPositive1(new int[]{3, 4, -1, 1}));
+        // 3
+        System.out.println(solution.firstMissingPositive1(new int[]{1, 2, 0}));
+        // 1
+        System.out.println(solution.firstMissingPositive1(new int[]{7, 8, 9, 11, 12}));
     }
 }
